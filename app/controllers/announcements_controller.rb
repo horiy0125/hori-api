a4004c0672d57607725602f4e9d0c1d30a664545
horri1520 <kh1520.shsf@gmail.com>
2021-06-16 03:07:05 +0900

    feat: implement index method

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,2 +1,10 @@
 class AnnouncementsController < ApplicationController
+
+  def index
+    @announcements = Announcement.order(created_at: :desc)
+  end
+
+  def new
+    @announcements = Announcement.new
+  end
 end
