c2943d579b43e059b15d5c65d9387ac3556738e6
horri1520 <kh1520.shsf@gmail.com>
2021-06-21 13:18:01 +0900

    feat: change order of announcements

--- app/controllers/api_v1/announcements_controller.rb
+++ app/controllers/api_v1/announcements_controller.rb
@@ -1,7 +1,7 @@
 class ApiV1::AnnouncementsController < ApplicationController
 
   def index
-    @announcements = Announcement.order(created_at: :desc)
+    @announcements = Announcement.order(published_at: :desc)
     render :json => @announcements
   end
 end
